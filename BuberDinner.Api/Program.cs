ab1458395d0092d78e597acd603dccf1fb4d2f53
usamaH-dotdev <hash.design.develop@gmail.com>
2024-07-26 22:33:35 -0500

    DI Changes

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,12 @@
+using BuberDinner.Application.Services;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+        
     builder.Services.AddControllers();
 }
 
---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+using BuberDinner.Application.Services.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthenticationResult Register(string firstName, string lastName, string email, string password);
+    AuthenticationResult Login(string email, string password);
+}
