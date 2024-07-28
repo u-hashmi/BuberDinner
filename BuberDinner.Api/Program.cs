f3f79b8ce7677c4139ed1bb46a1efe153ed0ccaa
usamaH-dotdev <hash.design.develop@gmail.com>
2024-07-27 19:23:54 -0500

    Finalized Changes

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,8 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
 using BuberDinner.Application.Services;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,10 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
         
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
